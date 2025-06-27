c05f386caca35c8b6fcb24426442e1f7a3650006
NoOneXXX <echo.crystal711@gmail.com>
2025-06-27 18:25:28 +0800

    打包命令

---
+++ build.py
@@ -0,0 +1,20 @@
+import os
+
+AUTHOR = 'Echo'
+VERSION = '1.0.0'
+app_name = 'KeepnotePlus'
+
+build_command = "nuitka --standalone --mingw64 --enable-plugin=pyside6 "
+build_command += "--windows-disable-console "
+build_command += "--output-dir=out "
+build_command += f"--windows-company-name={AUTHOR}  --windows-product-name={app_name} "
+build_command += f"--windows-product-version={VERSION} "
+build_command += "--follow-import-to=gui "
+build_command += "main.py  --lto=no "
+# ========
+# 运行pack_resources.py 如果这两个文件已经生成了就不要运行这个代码
+# 没有必要重复的生成，因为这样也会让已经修改的文件出现问题
+# ========
+# os.system("python pack_resources.py")
+print(build_command)
+os.system(build_command)  # 打包
